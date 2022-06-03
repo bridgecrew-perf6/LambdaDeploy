93c1c3ca1db5101bbd020969e1d6517ec7d12efa
EC2 Default User <ec2-user@ip-172-31-65-128.ec2.internal>
2022-06-03 19:34:58 +0000

    AutoDeploy

--- create_or_update_lambda.sh
+++ create_or_update_lambda.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 function create_or_update_lambda() {
     aws lambda list-functions >> res.json
     val1=$(grep $1 res.json)
@@ -9,8 +7,10 @@ function create_or_update_lambda() {
     length=${#val1}
     #echo $length
     if [ $length -gt 0 ]; then
+        
         aws lambda update-function-code --function-name  $1 --zip-file "fileb://TweetLoaderZIP.zip"
-    else
+    else#
+        
         aws lambda create-function --function-name $1 --runtime "python3.8" --role "arn:aws:iam::290544014146:role/LambdaRole" --zip-file "fileb://TweetLoaderZIP.zip" --handler lambda_function.lambda_handler
         
     fi
